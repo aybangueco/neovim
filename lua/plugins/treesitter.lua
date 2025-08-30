6272b8909208b83de6fa42a46580e40f5cb4c91b
aybangueco <203548283+aybangueco@users.noreply.github.com>
2025-08-30 23:49:56 +0800

    chore: add some more parser support for treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,30 @@ return {
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'css',
+        'scss',
+        'javascript',
+        'typescript',
+        'tsx',
+        'go',
+        'gomod',
+        'gosum',
+        'gotmpl',
+        'prisma',
+        'dockerfile',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -24,5 +47,5 @@ return {
     --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-  }
+  },
 }
