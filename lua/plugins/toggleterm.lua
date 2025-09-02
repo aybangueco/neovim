e55f094929ab98626a101ebb49d407110bb36ba7
aybangueco <203548283+aybangueco@users.noreply.github.com>
2025-09-02 16:14:43 +0800

    feat: add lazygit integration

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,5 +5,23 @@ return {
     opts = {
       open_mapping = [[<c-\>]],
     },
+    keys = {
+      {
+        '<leader>gl',
+        function()
+          local Terminal = require('toggleterm.terminal').Terminal
+          local lazygit = Terminal:new {
+            cmd = 'lazygit',
+            dir = 'git_dir',
+            direction = 'float',
+            float_opts = {
+              border = 'double',
+            },
+          }
+          return lazygit:toggle()
+        end,
+        desc = 'LazyGit',
+      },
+    },
   },
 }
