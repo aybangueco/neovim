a7e2c51ec88a0f6b15025fc7fcec849f95db9c55
aybangueco <203548283+aybangueco@users.noreply.github.com>
2025-09-03 15:58:08 +0800

    chore: remove toggleterm

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -37,20 +37,6 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
--- toggle term keymaps
-function _G.set_terminal_keymaps()
-  local opts = { buffer = 0 }
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
-end
-
-vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
-
 -- neotree keymaps
 vim.keymap.set('n', '\\', ':NvimTreeOpen<CR>', { desc = 'Open File Explorer', silent = true })
 vim.keymap.set('n', '|', ':NvimTreeClose<CR>', { desc = 'Close File Explorer', silent = true })
