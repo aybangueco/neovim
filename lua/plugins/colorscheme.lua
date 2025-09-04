1c3d8513125b5d9e45a709da269adfe784c17866
aybangueco <203548283+aybangueco@users.noreply.github.com>
2025-09-04 12:34:37 +0800

    feat: small changes in colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -19,9 +19,40 @@ return {
       require('onedark').setup {
         style = 'deep',
         transparent = true,
+        code_style = {
+          comments = 'italic',
+          keywords = 'none',
+          functions = 'bold',
+          strings = 'none',
+          variables = 'none',
+        },
       }
       -- Enable theme
       require('onedark').load()
+    end,
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      require('catppuccin').setup {
+        flavour = 'frappe',
+        transparent_background = true,
+        float = {
+          transparent = true,
+          solid = true,
+        },
+        no_italic = true,
+        term_colors = true,
+        dim_inactive = {
+          enabled = true,
+          shade = 'dark',
+          percentage = 0.80,
+        },
+        default_integrations = true,
+      }
+      -- vim.cmd.colorscheme 'catppuccin'
     end,
   },
 }
